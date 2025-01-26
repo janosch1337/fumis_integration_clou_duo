b801c88299e9e7ff653f965ca86926ad91bd4231
janosch1337 <48492924+janosch1337@users.noreply.github.com>
2025-01-26 20:00:32 +0100

    Update const.py

--- custom_components/fumis/const.py
+++ custom_components/fumis/const.py
@@ -2,9 +2,12 @@
 DOMAIN = "fumis"
 ATTR_POWER = "kw"
 ATTR_STATUS = "status"
+ATTR_STATUS_ID = "status_id"
 ATTR_ACTUAL_POWER = "actualpower"
 #ATTR_CONSUM_POWER = "kw"
 ATTR_FUEL = "fuel_quantity"
+ATTR_TEMPERATURE_BURN = "temperature_burn"
+
 
 MANUFACTURER = "fumis"
 
